1d27831d506b644dd13f55651c1ce6b4aa9e9917
NathalieRoos <54849936+NathalieRoos@users.noreply.github.com>
2021-09-14 15:20:44 +0200

    KNN regression exercise implemented
    All that's left to do is to compare it with linear regression, but I couldn't do that yet because I need your code for that.

--- practicals/code/knn.py
+++ practicals/code/knn.py
@@ -2,16 +2,17 @@ import numpy as np
 import scipy
 
 def knn_classifier(train_data, train_labels, test_data, k):
-    # Returns the labels for test_data, predicted by the k-NN
-    # clasifier trained on train_data and train_labels
-    # Input:
-    # train_data - num_train x p matrix with features for the training data
-    # train_labels - num_train x 1 vector with labels for the training data
-    # test_data - num_test x p matrix with features for the test data
-    # k - Number of neighbors to take into account (1 by default)
-    # Output:
-    # predicted_labels - num_test x 1 predicted vector with labels for the test data
-    
+    """ k-NN model for classification.
+        Returns the labels for test_data, as predicted by the k-NN classifier.
+        The label is determined by taking the mode of the labels of k-NN.
+    Inputs:
+        train_data: data to be trained on (num_train x p matrix)
+        train_labels: labels corresponding to the train data (num_train x 1 vector)
+        test_data: data for which to perform the knn (num_test x p matrix)
+        k: number of neighbours to be used (scalar)
+    Outputs:
+        Predicted labels for test_data (num_test x 1 vector)
+    """    
     D = scipy.spatial.distance.cdist(test_data, train_data, metric='euclidean')
     sort_ix = np.argsort(D, axis=1)
     sort_ix_k = sort_ix[:,:k] # Get the k smallest distances
@@ -20,6 +21,49 @@ def knn_classifier(train_data, train_labels, test_data, k):
 
     return predicted_labels
 
+def knn_regressor(train_data, train_targets, test_data, k):
+    """ k-NN model for regression.
+        Returns the targets for test_data, as predicted by the k-NN regressor.
+        The target is determined by taking the mean of the targets of k-NN.
+    Inputs:
+        train_data: data to be trained on (num_train x p matrix)
+        train_labels: targets corresponding to the train data (num_train x 1 vector)
+        test_data: data for which to perform the knn (num_test x p matrix)
+        k: number of neighbours to be used (scalar)
+    Outputs:
+        Predicted targets for test_data (num_test x 1 vector)
+    """
+    D = scipy.spatial.distance.cdist(test_data, train_data, metric='euclidean')
+    sort_ix = np.argsort(D, axis=1)
+    sort_ix_k = sort_ix[:,:k] # Get the k smallest distances
+    predicted_labels = train_targets[sort_ix_k]
+    predicted_labels = np.mean(predicted_labels, axis=1)
+
+    return predicted_labels
+
 def get_error_score(y_true, y_predicted):
+    """ Error score calculation for classification.
+        Checks if the labels correspond and gives a score. 
+        An error score of 1 means that everything is falsly predicted.
+        An error score of 0 means that everything is correctly predicted.
+    Inputs:
+        y_true: the true labels (num_datapoints x 1 vector)
+        y_predicted: the predicted labels (num_datapoints x 1 vector)
+    Outputs:
+        error score (scalar)
+    """
     correct = [i for i, j in zip(y_true, y_predicted) if i == j]
     return (len(y_true)-len(correct))/len(y_true)
+
+def get_mse(y_true, y_predicted):
+    """ Error score calculation for regression.
+        Calculates the mean squared error between the true targets and predicted targets. 
+        An lower mse is desired.
+    Inputs:
+        y_true: the true targets (num_datapoints x 1 vector)
+        y_predicted: the predicted targets (num_datapoints x 1 vector)
+    Outputs:
+        mean squared error (scalar)
+    """
+    mse = np.square(np.subtract(y_true, y_predicted)).mean()
+    return mse
